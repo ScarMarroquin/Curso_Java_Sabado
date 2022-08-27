2b76f0350a31dfcf326f3cbbb7dc35e8eec318f6
ScarMarroquin <marroquinmeli229mail.com>
2022-08-27 12:25:57 -0600

    first commit

---
+++ src/main/java/com/mycompany/curso_sabado_java/Curso_Sabado_Java.java
@@ -0,0 +1,18 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.curso_sabado_java;
+
+/**
+ *
+ * @author carin
+ */
+public class Curso_Sabado_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("HOLA VIOLET");
+    }
+}
